be853428ec7e5d71ba7cd2a9254737b23f87a195
Maxim Dzyubenko <ckraer@gmail.com>
2025-02-20 18:06:31 +0200

    Create Profile component

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,11 +1,11 @@
+import Profile from '../Profile/Profile';
 import userData from "../../userdata.json";
-
 
 function App() {
 
   return (
     <>
-      
+      <Profile profile={userData} />
     </>
   )
 }
