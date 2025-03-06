d84f6dcbb8e64752276af0c613f70b5d9ca54077
Maxim Dzyubenko <ckraer@gmail.com>
2025-03-06 16:47:14 +0200

    Add validation and ErrorMessage's

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -6,8 +6,8 @@ export default function Contact({ item: { id, name, number }, onDelete })
     return (
         <>
             <div className={css.wrapper}>
-                <p className={css.description}>{RiContactsFill}{name}</p>
-                <p className={css.description}>{RiPhoneFill}{number}</p>
+                <p className={css.description}><RiContactsFill />{name}</p>
+                <p className={css.description}><RiPhoneFill />{number}</p>
             </div>
             <button type="button" onClick={() => onDelete(id)}>Delete</button>
         </>
--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -1,5 +1,6 @@
-import { Formik, Form, Field } from 'formik'
+import { Formik, Form, Field, ErrorMessage } from 'formik'
 import css from './ContactForm.module.css'
+import * as Yup from 'yup'
 
 export default function ContactForm({addContact})
 {
@@ -10,17 +11,25 @@ export default function ContactForm({addContact})
         });
         actions.resetForm();
     };
+
+    const contactSchema = Yup.object().shape({
+        name: Yup.string().min(3, "Too short!").max(50, "Too long!").required("Field cannot be empty!"),
+        number: Yup.string().min(3, "Too short!").max(50, "Too long!").required("Field cannot be empty!")
+    });
+
     return (
         <Formik initialValues={{
             name: '',
             number: ''
-        }} onSubmit={handleSubmit}>
+        }} onSubmit={handleSubmit} validationSchema={contactSchema}>
             <Form className={css.form}>
                 <label className={css.label}>
                     Name <Field type="text" name="name" />
+                    <ErrorMessage name="name" component="p" className={css.error} />
                 </label>
                 <label className={css.label}>
                     Number <Field type="tel" name="number" />
+                    <ErrorMessage name="number" component="p" className={css.error} />
                 </label>
                 <button className={css.button} type="submit">Add contact</button>
             </Form>
