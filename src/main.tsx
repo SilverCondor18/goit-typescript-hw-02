ee9d6a36f3b6df9ba347996071e6ec1a09e139d4
Maxim Dzyubenko <ckraer@gmail.com>
2025-04-25 17:07:27 +0300

    Prepare for react ts

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,11 @@
-import { StrictMode } from 'react'
+import React from 'react'
 import { createRoot } from 'react-dom/client'
 import 'modern-normalize'
 import './index.css'
 import App from './components/App/App'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
+createRoot(document.getElementById('root') as HTMLElement).render(
+  <React.StrictMode>
     <App />
-  </StrictMode>,
-)
+  </React.StrictMode>,
+);
